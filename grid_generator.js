3f319d52edfb402d988b9fbd5dc7413cd53a860e
joshua.tang <joshua.l.tang@ibm.com>
2018-02-23 21:42:33 -0500

    grid working and creating input array

--- grid_generator.js
+++ grid_generator.js
@@ -1,9 +1,21 @@
-var selected = [];
+var height = 10
+var width = 20
+var selected = []
+var input = Array.apply(null, Array(height)).map(Number.prototype.valueOf, 0)
 
-var grid = fillableGrid(10,10,function(el,row,col,i) {
+var raytracer
+var grid = fillableGrid(height,width,function(el,row,col,i) {
+    if(selected.includes(i)) {
+        el.className = '';
+        selected.splice(selected.indexOf(i), 1);
+        input[height-1-row] -= Math.pow(2, width-col-1);
+    } else {
+        el.className = 'selected';
+        selected.push(i);
+        input[height-1-row] += Math.pow(2, width-col-1);
+    }
     console.log("You've selected:", selected.toString());
-    el.className = 'selected';
-    selected.push(i);
+    console.log("The input will be:", input.toString());
 });
 
 document.body.appendChild(grid);
@@ -16,6 +28,7 @@ function fillableGrid(rows, cols, callback) {
         var tr = grid.appendChild(document.createElement('tr'));
         for(var c=0; c<cols; ++c) {
             var cell = tr.appendChild(document.createElement('td'));
+            i++;
             cell.addEventListener('click', (function(el, r, c, i) {
                 return function() {
                     callback(el,r,c,i);
